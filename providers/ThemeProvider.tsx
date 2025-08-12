395ebac3c064e80914b68de418ad908ca54088cb
Tarun Chaudhary <imtarunchaudhary2@gmail.com>
2025-08-12 08:59:40 +0530

    updated themeprovider file

--- providers/ThemeProvider.tsx
+++ providers/ThemeProvider.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ReactNode } from "react";
 
-export function ThemeProvider({
-  children,
-  ...props
-}: ThemeProviderProps & { children: ReactNode }) {
+interface Props {
+  children: ReactNode;
+  // Add any props you want to pass to NextThemesProvider here if needed
+}
+
+export function ThemeProvider({ children, ...props }: Props) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
