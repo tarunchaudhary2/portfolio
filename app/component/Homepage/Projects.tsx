f86c523c517026c5c9db82e53f1aac2fe16fb6c8
Tarun Chaudhary <imtarunchaudhary2@gmail.com>
2025-08-13 11:22:45 +0530

    Added todo project image

--- app/component/Homepage/Projects.tsx
+++ app/component/Homepage/Projects.tsx
@@ -91,7 +91,7 @@ const Projects = () => {
       icon: <Logo />,
       title: "Portfolio",
       description:
-        "Architect and manage scalable cloud environments on Azure and AWS using Infrastructure as Code (Terraform, Bicep, ARM Templates, AWS CloudFormation) to automate provisioning and reduce deployment time",
+        "Developed a personal portfolio website highlighting my Experience, Services, Skills, Projects and certifications. This website is developed with tailwind css with responsive design compatible with Desktop, tablets and Mobiles. The Website is hosted on vercel app which is a free hosting platform for a certain amount of time for portfolios websites.",
       image: "/images/portfolioss.png",
       githubLink: "https://github.com/tarunchaudhary2/portfolio",
       dribbbleLink: "http://3.111.170.116:3000/",
